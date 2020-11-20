54ad40f558c9f0de0cf39ebbe4e1e93686274be7
sejaldave088 <sejaldave088@gmail.com>
2020-11-19 21:02:44 -0500

    It was remaining,,

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -9,22 +9,45 @@ namespace ConsoleApp1
             Adult ad = new Adult();
             ad.FirstName = "prutha";
             ad.LastName = "biho";
+            Child cd = new Child();
             Console.WriteLine("the name is:", ad.FirstName);
             Console.WriteLine("the name is:", ad.LastName);
-
+            polymor[] a1 = new polymor[] { ad,cd };
 
         }
-        public class Adult
+        public interface polymor
         {
 
-            public string FirstName;
-            public string LastName;
-            
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public void Print()
+            {
+                Console.WriteLine(FirstName);
+                Console.WriteLine(LastName);
+            }
+
         }
-        public class Child
+        public class Adult : polymor
         {
-            public string FirstName;
-            public string LastName;
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public void Print()
+            {
+                // Code to draw a circle...
+                Console.WriteLine("display a name");
+
+            }
+        }
+            public class Child : polymor
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public void Print()
+            {
+                // Code to draw a circle...
+                Console.WriteLine("display a name");
+
+            }
         }
     }
 }
